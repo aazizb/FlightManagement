43bbf178aa822ba3c6a97f77ca34ae3620289589
aazizb <aazizb@hotmail.com>
2023-06-29 23:23:53 +0200

    Injected in the application pipeline: repositories and services

---
+++ Server/Extensions/IServiceCollectionsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FlightManagement.Server.Repositories;
+using FlightManagement.Server.Repositories.Contracts;
+using FlightManagement.Server.Services;
+using FlightManagement.Server.Services.Contracts;
+
+namespace FlightManagement.Server.Extensions
+{
+    public static class IServiceCollectionsExtensions
+    {
+        private static void ConfigureRepositoryManager(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+        private static void ConfigureServiceManager(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+        }
+
+        public static void AddServerSideServices(this IServiceCollection services)
+        {
+            services.ConfigureRepositoryManager();
+            services.ConfigureServiceManager();
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,42 +1,50 @@
 using FlightManagement.Server.Data;
+using FlightManagement.Server.Extensions;
 
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+{
+    // Add services to the container.
 
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
 
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddServerSideServices();
+}
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+
+    app.MapRazorPages();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+
+    app.Run();
+
+
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
