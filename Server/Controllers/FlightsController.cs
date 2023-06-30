ae7e9ee6380d8cd95c02329d2c71cec3aa1184be
aazizb <aazizb@hotmail.com>
2023-06-30 07:23:41 +0200

    Implemented flight endpoints

---
+++ Server/Controllers/FlightsController.cs
@@ -0,0 +1,70 @@
+﻿using FlightManagement.Server.Services.Contracts;
+using FlightManagement.Shared;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        private readonly IServiceManager service;
+
+        public FlightsController(IServiceManager service)
+        {
+            this.service = service;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var flights = service.FlightService.GetAll(false);
+                return Ok(flights);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("{id:int}", Name = "GetBy")]
+        public IActionResult GetBy(int id)
+        {
+            Flight flight = service.FlightService.GetFlightBy(id, false);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(flight);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] Flight flight)
+        {
+            if (flight is null)
+            {
+                return BadRequest();
+            }
+            var entity = service.FlightService.CreateFlight(flight);
+            return CreatedAtRoute("GetBy", new { id = entity.Id }, entity);
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            service.FlightService.DeleteFlight(id, false);
+            return NoContent();
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult Update(int id, [FromBody] Flight flight)
+        {
+            if (flight == null)
+            {
+                return BadRequest();
+            }
+            service.FlightService.UpdateFlight(id, flight);
+            return NoContent();
+        }
+
+    }
+}
