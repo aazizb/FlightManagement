f75d122a0d13191014858b9bf8456ee05745063c
aazizb <aazizb@hotmail.com>
2023-06-30 07:15:06 +0200

    Implemented flight service

--- Server/Services/Contracts/IFlightService.cs
+++ Server/Services/Contracts/IFlightService.cs
@@ -1,6 +1,13 @@
-﻿namespace FlightManagement.Server.Services.Contracts
+﻿using FlightManagement.Shared;
+
+namespace FlightManagement.Server.Services.Contracts
 {
     public interface IFlightService
     {
+        IEnumerable<Flight> GetAll(bool trackchanges);
+        Flight GetFlightBy(int id, bool trackchanges);
+        Flight CreateFlight(Flight flight);
+        void DeleteFlight(int id, bool trackchanges);
+        void UpdateFlight(int id, Flight flight);
     }
 }
--- Server/Services/FlightService.cs
+++ Server/Services/FlightService.cs
@@ -1,5 +1,6 @@
 ﻿using FlightManagement.Server.Repositories.Contracts;
 using FlightManagement.Server.Services.Contracts;
+using FlightManagement.Shared;
 
 namespace FlightManagement.Server.Services
 {
@@ -10,6 +11,36 @@ namespace FlightManagement.Server.Services
         public FlightService(IRepositoryManager repository)
         {
             this.repository = repository;
+        }
+
+        public Flight CreateFlight(Flight flight)
+        {
+            repository.Flight.CreateFlight(flight);
+            repository.Save();
+            return flight;
+        }
+
+        public void DeleteFlight(int id, bool trackchanges)
+        {
+            var entity = repository.Flight.GetFlightById(id, trackchanges);
+            repository.Flight.DeleteFlight(entity);
+            repository.Save();
+        }
+
+        public IEnumerable<Flight> GetAll(bool trackchanges)
+        {
+            return repository.Flight.GetFlights(trackchanges);
+        }
+
+        public Flight GetFlightBy(int id, bool trackchanges)
+        {
+            return repository.Flight.GetFlightById(id, trackchanges);
+        }
+
+        public void UpdateFlight(int id, Flight flight)
+        {
+            repository.Flight.UpdateFlight(flight);
+            repository.Save();
         }
     }
 }
