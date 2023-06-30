2ff1050ba3be72712aca632ce56bed48cdde7206
aazizb <aazizb@hotmail.com>
2023-06-30 07:02:09 +0200

    Implemented flight repository

--- Server/Repositories/Contracts/IFlightRepository.cs
+++ Server/Repositories/Contracts/IFlightRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace FlightManagement.Server.Repositories.Contracts
+﻿using FlightManagement.Shared;
+
+namespace FlightManagement.Server.Repositories.Contracts
 {
     public interface IFlightRepository
     {
+        IEnumerable<Flight> GetFlights(bool trackchanges);
+        Flight GetFlightById(int id, bool trackchanges);
+        void CreateFlight(Flight flight);
+        void DeleteFlight(Flight flight);
+        void UpdateFlight(Flight flight);
     }
 }
--- Server/Repositories/FlightRepository.cs
+++ Server/Repositories/FlightRepository.cs
@@ -9,5 +9,32 @@ namespace FlightManagement.Server.Repositories
         public FlightRepository(AppDbContext context) : base(context)
         {
         }
+
+        public void CreateFlight(Flight flight)
+        {
+            Create(flight);
+        }
+
+        public void DeleteFlight(Flight flight)
+        {
+            Delete(flight);
+        }
+
+        public Flight GetFlightById(int id, bool trackchanges)
+        {
+            return FindBy(o => o.Id.Equals(id), trackchanges)
+                .SingleOrDefault();
+        }
+
+        public IEnumerable<Flight> GetFlights(bool trackchanges)
+        {
+            return FindAll(trackchanges).OrderBy(o => o.Id)
+                .ToList();
+        }
+
+        public void UpdateFlight(Flight flight)
+        {
+            Update(flight);
+        }
     }
 }
